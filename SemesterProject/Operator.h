402881face13e5fa609058566242de402fc399fc
danksalot <danksalot@users.noreply.github.com>
2017-10-24 09:59:19 -0500

    Create Operator.h

---
+++ SemesterProject/Operator.h
@@ -0,0 +1,22 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+ 
+#include "Token.h"
+ 
+class Operator
+    {
+    public:
+        enum Precedence {
+                        BinaryPlusMinusPrecedence,
+                        MultiplyDividePrecedence,
+                        UnaryPlusMinusPrecedence,
+                        ParenthesesPrecedence
+                        };
+    private:
+                    Operator    ();
+                    Operator    (const Operator &);
+                    ~Operator   ();
+        Operator &  operator =  (const Operator &);
+    };
+ 
+#endif
