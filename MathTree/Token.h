c12461ea055b2cd51bbb74fd4fdc06a938974dc4
danksalot <danksalot@users.noreply.github.com>
2017-10-24 09:34:08 -0500

    Create Token.h

---
+++ MathTree/Token.h
@@ -0,0 +1,38 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+class Token
+	{
+	public:
+		typedef	long			NumberType;
+		typedef unsigned long	ULONG;
+		enum	TokenType	{
+							OperatorPlus,
+							OperatorMinus,
+							OperatorMultiply,
+							OperatorDivide,
+							Constant,
+							Variable
+							};
+		enum PrecedenceType	{
+							AddSubtractPrecedence,
+							MultDividePrecedence,
+							UnaryPlusMinusPrecedence
+							};
+				Token	();
+				Token	(const Token &);
+				Token	(TokenType, NumberType);
+				~Token	();
+		Token &	operator =	(const Token &);
+
+		bool				IsLeftSideDone;
+		TokenType			Type;
+		union
+			{
+			NumberType		Value;
+			PrecedenceType	Precedence;
+			ULONG			Which;
+			};
+	};
+
+#endif
