93b803369ffc6795eaeabc76709d6f8f729a11c3
danksalot <danksalot@users.noreply.github.com>
2017-10-24 09:33:32 -0500

    Create MathTree.h

---
+++ MathTree/MathTree.h
@@ -0,0 +1,42 @@
+#ifndef MATH_TREE_H
+#define MATH_TREE_H
+
+#include "Token.h"
+
+class MathTree
+	{
+	public:
+		struct Node
+			{
+			Node	(const Token &);
+
+			Token	Toke;
+			Node *	pParent;
+			Node *	pLeft;
+			Node *	pRight;
+			};
+		struct Variable
+			{
+			Token::NumberType	Value;
+			bool				IsDefined;
+			};
+
+							MathTree				();
+							~MathTree				();
+		void				InsertOperand			(const Token &);
+		void				InsertBinaryOperator	(const Token &);
+		void				InsertUnaryOperator		(const Token &);
+		Node *				GetLeftMostNode			(Node *);
+		Token::NumberType	GetValue				(Node *);
+		Token::NumberType	Evaluate				();
+	private:
+					MathTree	(const MathTree &);
+		MathTree &	operator =	(const MathTree &);
+
+		Node *		pRoot;
+		Node *		pLastOperator;
+		Variable	Vars [100];
+
+	};
+
+#endif
